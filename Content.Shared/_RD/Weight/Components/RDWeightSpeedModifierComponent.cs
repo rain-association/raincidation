6b1d7f2342db80e6078eabed09a05d0b7c64f61e
Tornado Tech <tornado.the.technologies@gmail.com>
2025-05-03 15:22:12 +1000

    feat: weight speed modifier

---
+++ Content.Shared/_RD/Weight/Components/RDWeightSpeedModifierComponent.cs
@@ -0,0 +1,37 @@
+﻿using Content.Shared._RD.Weight.Systems;
+using Robust.Shared.GameStates;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._RD.Weight.Components;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState(fieldDeltas: true)]
+[Access(typeof(RDWeightSpeedModifierSystem), Other = AccessPermissions.None)]
+public sealed partial class RDWeightSpeedModifierComponent : Component
+{
+    [ViewVariables, AutoNetworkedField]
+    public float Value = 1;
+
+    [DataField, ViewVariables, AutoNetworkedField]
+    public RDWeightSpeedModifierCurve Curve = new RDWeightSpeedModifierLinearCurve();
+}
+
+[ImplicitDataDefinitionForInheritors]
+public abstract partial class RDWeightSpeedModifierCurve
+{
+    public abstract float Calculate(Entity<RDWeightComponent> entity, float total);
+}
+
+[Serializable, NetSerializable]
+public sealed partial class RDWeightSpeedModifierLinearCurve : RDWeightSpeedModifierCurve
+{
+    [DataField]
+    public float Min;
+
+    [DataField]
+    public float Max;
+
+    public override float Calculate(Entity<RDWeightComponent> entity, float total)
+    {
+        return Math.Clamp(1 - (total - Min) / (Max - Min), 0f, 1f);
+    }
+}
---
+++ Content.Shared/_RD/Weight/Systems/RDWeightSpeedModifierSystem.cs
@@ -0,0 +1,36 @@
+﻿using Content.Shared._RD.Mathematics.Extensions;
+using Content.Shared._RD.Weight.Components;
+using Content.Shared._RD.Weight.Events;
+using Content.Shared.Movement.Systems;
+
+namespace Content.Shared._RD.Weight.Systems;
+
+public sealed class RDWeightSpeedModifierSystem : EntitySystem
+{
+    [Dependency] private readonly MovementSpeedModifierSystem _movementSpeedModifierSystem = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<RDWeightSpeedModifierComponent, RDWeightRefreshEvent>(OnRefresh);
+        SubscribeLocalEvent<RDWeightSpeedModifierComponent, RefreshMovementSpeedModifiersEvent>(OnSpeedRefresh);
+    }
+
+    private void OnRefresh(Entity<RDWeightSpeedModifierComponent> entity, ref RDWeightRefreshEvent args)
+    {
+        var value = entity.Comp.Curve.Calculate(args.Entity, args.Total);
+        if (value.AboutEquals(entity.Comp.Value))
+            return;
+
+        entity.Comp.Value = value;
+        DirtyField(entity, entity.Comp, nameof(RDWeightSpeedModifierComponent.Value));
+
+        _movementSpeedModifierSystem.RefreshMovementSpeedModifiers(entity);
+    }
+
+    private void OnSpeedRefresh(Entity<RDWeightSpeedModifierComponent> entity, ref RefreshMovementSpeedModifiersEvent args)
+    {
+        args.ModifySpeed(entity.Comp.Value);
+    }
+}
