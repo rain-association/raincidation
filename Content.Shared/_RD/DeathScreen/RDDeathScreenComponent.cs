3b973cad7d537a8d24a5863942f3100663923eac
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-05-10 21:43:28 +1000

    feat: death screen

---
+++ Content.Shared/_RD/DeathScreen/RDDeathScreenComponent.cs
@@ -0,0 +1,6 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._RD.DeathScreen;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class RDDeathScreenComponent : Component;
