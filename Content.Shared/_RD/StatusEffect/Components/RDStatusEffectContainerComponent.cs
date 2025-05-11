c5ed75d937dab225b4060495d565218220a25b4a
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-05-11 20:40:11 +1000

    feat: port status effects

---
+++ Content.Shared/_RD/StatusEffect/Components/RDStatusEffectContainerComponent.cs
@@ -0,0 +1,12 @@
+﻿using Content.Shared._RD.StatusEffect.Systems;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._RD.StatusEffect.Components;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState(true)]
+[Access(typeof(RDStatusEffectSystem))]
+public sealed partial class RDStatusEffectContainerComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public HashSet<EntityUid> ActiveStatusEffects = new();
+}
---
+++ Content.Shared/_RD/StatusEffect/Events/RDBeforeStatusEffectAddedEvent.cs
@@ -0,0 +1,9 @@
+﻿using Robust.Shared.Prototypes;
+
+namespace Content.Shared._RD.StatusEffect.Events;
+
+/// <summary>
+/// Raised on an entity before a status effect is added to determine if adding it should be cancelled.
+/// </summary>
+[ByRefEvent]
+public record struct RDBeforeStatusEffectAddedEvent(EntProtoId Effect, bool Cancelled = false);
