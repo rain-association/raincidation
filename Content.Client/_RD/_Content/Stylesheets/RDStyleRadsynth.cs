cfac647e1fcfc2e40e19ba901f0e0bb4aaa08ada
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-05-16 16:50:41 +1000

    wip: custom fonts

--- Content.Client/_RD/_Content/Stylesheets/RDStyleRadsynth.cs
+++ Content.Client/_RD/_Content/Stylesheets/RDStyleRadsynth.cs
@@ -4,6 +4,8 @@ using Content.Client.Resources;
 using Robust.Client.ResourceManagement;
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
+using Robust.Client.UserInterface.CustomControls;
+using static Robust.Client.UserInterface.StylesheetHelpers;
 
 namespace Content.Client._RD._Content.Stylesheets;
 
@@ -13,16 +15,38 @@ public sealed class RDStyleRadsynth : RDStyle
 
     public RDStyleRadsynth(IResourceCache resCache) : base(resCache)
     {
-        var checkBoxTextureChecked = resCache.GetTexture("/Textures/_RD/Interface/Styles/Radsynth/checkbox_checke.png");
+        var fontCozetteVector = resCache.GetFont("/Fonts/_RD/Cozette/vector.otf", size: 18);
+
+        var checkBoxTextureChecked = resCache.GetTexture("/Textures/_RD/Interface/Styles/Radsynth/checkbox_checked.png");
         var checkBoxTextureUnchecked = resCache.GetTexture("/Textures/_RD/Interface/Styles/Radsynth/checkbox.png");
 
         Stylesheet = new Stylesheet(base.Stylesheet.Rules.Concat([
+            Element()
+                .Class("monospace")
+                .Prop("font", fontCozetteVector),
+
+            #region Window title.
+            new StyleRule(
+                new SelectorElement(typeof(Label), [DefaultWindow.StyleClassWindowTitle], null, null),
+                new[]
+                {
+                    new StyleProperty(Label.StylePropertyFontColor, NanoGold),
+                    new StyleProperty(Label.StylePropertyFont, fontCozetteVector),
+                }),
+            #endregion
+
+            #region Checkbox
             new (new SelectorElement(typeof(TextureRect), [CheckBox.StyleClassCheckBox], null, null),
                 [new StyleProperty(TextureRect.StylePropertyTexture, checkBoxTextureUnchecked)]
             ),
             new (new SelectorElement(typeof(TextureRect), [CheckBox.StyleClassCheckBox, CheckBox.StyleClassCheckBoxChecked], null, null),
                 [new StyleProperty(TextureRect.StylePropertyTexture, checkBoxTextureChecked)]
             ),
+            #endregion
+
+            Element<RichTextLabel>()
+                .Prop(Label.StylePropertyFont, fontCozetteVector),
+
         ])
         .ToList());
     }
