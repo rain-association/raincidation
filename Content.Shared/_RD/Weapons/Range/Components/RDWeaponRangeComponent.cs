05314dc5ffee9296c94043345597b8652de6241e
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-06-04 18:34:25 +1000

    feat: some content

---
+++ Content.Shared/_RD/Weapons/Range/Components/RDWeaponRangeComponent.cs
@@ -0,0 +1,10 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._RD.Weapons.Range.Components;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState(fieldDeltas: true)]
+public sealed partial class RDWeaponRangeComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public float F;
+}
