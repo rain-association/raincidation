8714d05dd0e6df4ed511fb84d501356ea82189b3
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-07-04 08:19:32 +1000

    feat: weight

---
+++ Content.Shared/_RD/Weight/Components/RDWeightExamineCoponent.cs
@@ -0,0 +1,13 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._RD.Weight.Components;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState(fieldDeltas: true)]
+public sealed partial class RDWeightExamineCoponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public LocId? Current;
+
+    [DataField, AutoNetworkedField]
+    public Dictionary<LocId, Range> Examines = new();
+}
---
+++ Content.Shared/_RD/Weight/Systems/RDWeightExamineSystem.cs
@@ -0,0 +1,45 @@
+﻿using Content.Shared._RD.Weight.Components;
+using Content.Shared._RD.Weight.Events;
+using Content.Shared.Examine;
+
+namespace Content.Shared._RD.Weight.Systems;
+
+public sealed class RDWeightExamineSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<RDWeightExamineCoponent, ExaminedEvent>(OnExamined);
+        SubscribeLocalEvent<RDWeightExamineCoponent, RDWeightRefreshEvent>(OnRefresh);
+    }
+
+    private void OnExamined(Entity<RDWeightExamineCoponent> entity, ref ExaminedEvent args)
+    {
+        if (entity.Comp.Current is null)
+            return;
+
+        using (args.PushGroup(nameof(RDWeightExamineCoponent), 1))
+        {
+            args.PushMarkup(Loc.GetString("mc-weight-examine", ("name", Loc.GetString(entity.Comp.Current))));
+        }
+    }
+
+    private void OnRefresh(Entity<RDWeightExamineCoponent> entity, ref RDWeightRefreshEvent args)
+    {
+        var previous = entity.Comp.Current;
+        LocId? current = null;
+
+        foreach (var (id, range) in entity.Comp.Examines)
+        {
+            if (args.Total <= range.Max && args.Total >= range.Min)
+                current = id;
+        }
+
+        if (previous == current)
+            return;
+
+        entity.Comp.Current = current;
+        DirtyField(entity, entity.Comp, nameof(RDWeightExamineCoponent.Current));
+    }
+}
